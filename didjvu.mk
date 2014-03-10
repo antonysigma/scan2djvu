11318214911f88a170c816f507fe3361c556bebc
Antony Chan <cschan@eee.hku.hk>
2014-03-10 22:53:47 +0800

    Write some codes to convert each djvu page to a mixed raster content PDF

--- didjvu.mk
+++ didjvu.mk
@@ -3,14 +3,36 @@ SHELL:=/bin/bash
 prefix=out
 dpi=600
 
-djvu_pages=$(subst .tif,.djvu,$(wildcard $(prefix)/*.tif))
+tmp=/dev/shm
+ext=tif
+
+djvu_pages=$(subst .$(ext),.djvu,$(wildcard $(prefix)/*.$(ext)))
+pdf_pages=$(subst .djvu,.pdf,$(djvu_pages))
 
 all:$(prefix).djvu
 
 $(prefix).djvu:$(djvu_pages)
 	djvm -c $@ $^
-#	ocrodjvu -j4 -etesseract -leng --in-place $@
+	ocrodjvu -j4 -etesseract -leng --in-place $@
 
-$(djvu_pages):%.djvu:%.tif
+$(djvu_pages):%.djvu:%.$(ext)
 	mogrify -trim -normalize -compress lzw $<
 	didjvu encode --loss-level 200 -m djvu -d $(dpi) $< -o $@
+	
+###############################################################
+pdf:$(prefix)-converted.pdf
+
+$(prefix)-converted.pdf:$(pdf_pages)
+	gs -dNOPAUSE -dBATCH -sOUTPUTFILE=$@ -sDEVICE=pdfwrite $^
+
+$(pdf_pages):%.pdf:%.djvu
+#	Extract layers
+	ddjvu -mode=background -format=tiff $< $(tmp)/$(notdir $*)-bg.tif
+	ddjvu -mode=mask -page=1 -format=pbm $< $(tmp)/$(notdir $*)-bw.pbm
+#	ddjvu -mode=foreground -page=1 -format=pdf $< $*-ft.pdf
+#	Convert foreground to best format
+	convert $(tmp)/$(notdir $*)-bw.pbm -threshold 1 -transparent white -compress jbig2 $(tmp)/$(notdir $*)-bw.pdf
+	convert $(tmp)/$(notdir $*)-bg.tif -compress jpeg2000 $(tmp)/$(notdir $*)-bg.pdf
+#	Merge layers
+	pdftk $(tmp)/$(notdir $*)-bw.pdf multibackground $(tmp)/$(notdir $*)-bg.pdf $@
+	rm $(tmp)/$(notdir $*)-*
